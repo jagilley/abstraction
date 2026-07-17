cada7d10c55d7d9b12a68788862d56ef9fdceb1c
Jasper <37783831+jagilley@users.noreply.github.com>
2026-07-17 14:20:00 -0700

    major reorgs

---
+++ experiments/rhm/ratchet/__init__.py
@@ -0,0 +1,5 @@
+"""RHM ratchet / meta-learning arc (wake-sleep, RL, FOMAML, meta-learning).
+
+Abandoned line: the wake-sleep ratchet replicates MNIST dynamics on RHM but never
+compounds. See README.md in this directory. Moved here 2026-07-17.
+"""
